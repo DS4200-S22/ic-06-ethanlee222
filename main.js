7ac1ac0b5686d4dd4fca68ecc068eeb7980a0600
Ethan Lee <lee.et@northeastern.edu>
2022-02-10 13:22:54 -0500

    done

--- main.js
+++ main.js
@@ -8,7 +8,14 @@ Modified: 12/06/21
 
 //Starter code for Button 1 click
     // You will need to call this function when Button 1 is clicked
-    // You will need to modiffy the body of this function as described in the assignment Readme
+    // You will need to modify the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    // let newText = "Button was clicked1";
+    let randNum = Math.random();
+
+    let button_div = document.getElementById("button-div");
+
+    button_div.innerHTML = randNum;
+
+    console.log(button_div)
 }
